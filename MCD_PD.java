f62d80c62061b6ea21da47e3c47fbac9294335c9
Juan <juanyoung41@gmail.com>
2022-07-11 11:46:15 +0700

    Update Fixed

--- MCD_PD.java
+++ MCD_PD.java
@@ -1,10 +1,10 @@
 import java.util.*;
+import java.util.stream.Stream;
 
 public class MCD_PD{
 
     public static ArrayList MatrixChainOrder(int p[]){
         int n = p.length-1;
-        int j;
         int m [][] = new int[n+1][n+1];
         int s [][] = new int[n+1][n+1];
 
@@ -14,10 +14,10 @@ public class MCD_PD{
 
         for(int l = 2; l <= n ; l++){
             for(int i = 1; i <= n-l+1;i++){
-                j = i+l-1;
+                int j = i+l-1;
                 m[i][j] = Integer.MAX_VALUE;
                 for(int k = i ; k <= j-1;k++){
-                    int q = m[i][k] + m[k+1][j]+p[i-1]*p[k]+p[j];
+                    int q = m[i][k] + m[k+1][j] + p[i-1] * p[k] * p[j];
                     if (q < m[i][j]){
                         m[i][j] = q;
                         s[i][j] = k;
@@ -34,11 +34,11 @@ public class MCD_PD{
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Masukan Array p[] (Misal User Input = 5, 3, 8, 9, 19)");
+        System.out.println("Masukan Array p[] (Misal User Input = 5 3 8 9 19)");
         String input = in.nextLine();
-        
-        int p[] = {5, 3, 8, 9, 10, 35, 50,28};
-        ArrayList <int[][]> arr2 = MatrixChainOrder(p);
+        String splited[] = input.split(" ");
+        int[] values = Stream.of(splited).mapToInt(Integer::parseInt).toArray();
+        ArrayList <int[][]> arr2 = MatrixChainOrder(values);
         int a[][] = arr2.get(0);
         int b[][] = arr2.get(1);
 
@@ -48,8 +48,7 @@ public class MCD_PD{
             }
             System.out.println();
         }
-        System.out.println();
-        System.out.println();
+        System.out.println("\n");
         for(int i = 1 ; i < b.length-1 ; i++){
             for(int j = 1; j < b[i].length ; j++){
                 System.out.print(b[i][j]+"\t");
